e840fd04560f54f26f82638a2497f7a0c172bdcb
Rabiu Muhammad Nasir <111989601+DarkVayder@users.noreply.github.com>
2024-09-19 10:30:39 +0100

    feat: adjusted padding

--- frontend/src/components/RelatedProducts.jsx
+++ frontend/src/components/RelatedProducts.jsx
@@ -23,7 +23,7 @@ const RelatedProducts = ({ category, subCategory }) => {
   return (
     <div>
       <div className='my-24'>
-        <div className='text-center text-3xl py-20'>
+        <div className='text-center text-3xl py-2'>
           <Title text1={'RELATED'} text2={'PRODUCTS'} />
         </div>
       </div>
