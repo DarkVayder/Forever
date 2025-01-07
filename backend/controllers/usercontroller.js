bf8633d066e57388aca50086fd8ea569aedd8629
Rabiu Muhammad Nasir <111989601+DarkVayder@users.noreply.github.com>
2025-01-07 16:15:45 +0100

    feat: added logic for routes

---
+++ backend/controllers/usercontroller.js
@@ -0,0 +1,21 @@
+
+//Route for login user
+const loginUser = async (req, res) => {
+    
+    
+
+}
+
+//Route for register new user
+const registerUser = async (req, res) => {
+
+    
+
+}
+
+//Route for Admin login
+const adminLogin = async (req, res) => {
+
+}
+
+export { loginUser, registerUser, adminLogin }
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
+import userRouter from './routes/userRoute.js'
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -13,7 +14,8 @@ connectCloudinary()
 app.use(express.json())
 app.use(cors())
 
-// Mock API for products
+// Mock API for endpoints
+app.use('/api/user',userRouter)
 
 app.get('/',(req, res) => {
     res.send('API is working!!')
