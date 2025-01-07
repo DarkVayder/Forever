d88f3fd059158a105c3b4ec5463fc3ad87370979
Rabiu Muhammad Nasir <111989601+DarkVayder@users.noreply.github.com>
2025-01-07 15:08:55 +0100

    feat: added the db file and logic

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDB from './config/mongodb.js'
 
 const app = express()
 const port = process.env.PORT || 5000
+connectDB()
 
 //Middle ware
 app.use(express.json())
