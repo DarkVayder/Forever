3cbafd0e38064505870157fd4a42410cd3a911de
Rabiu Muhammad Nasir <111989601+DarkVayder@users.noreply.github.com>
2025-01-08 11:29:27 +0100

    feat: npm i multer dependency package

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
 import {listProducts, removeProduct, singleProduct, addProduct} from "../controllers/productController.js"
+import upload from "../middleware/multer.js"
 
 const productRouter = express.Router();
 
 productRouter.get('/listProducts', listProducts);
-productRouter.post('/add', addProduct);
+productRouter.post('/add',upload.fields([{name:'image1',maxCount:1},{name:'image2',maxCount:1},{name:'image3',maxCount:1},{name:'image4',maxCount:1}]), addProduct);
 productRouter.post('/remove', removeProduct);
 productRouter.post('/single', singleProduct);
 
